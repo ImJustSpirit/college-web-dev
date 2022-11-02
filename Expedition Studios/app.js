35ee5cbd5ee913a0b3cc7976ec43692ce48d04d7
Jack Petty <77806303+ImJustSpirit@users.noreply.github.com>
2022-11-02 11:42:09 +0000

    SweetAlert2
    Messed around with and implemented SweetAlert2.

--- Expedition Studios/app.js
+++ Expedition Studios/app.js
@@ -1,7 +1,15 @@
-const splash = document.querySelector('.splash');
+const splash = document.querySelector(".splash");
 
-document.addEventListener('DOMContentLoaded', (e)=>{
+document.addEventListener("DOMContentLoaded", (e)=>{
     setTimeout(()=>{
-        splash.classList.add('display-none');
+        splash.classList.add("display-none");
     }, 2000);
 })
+
+function login(){
+    Swal.fire({
+        icon: "error",
+        title: "Well...",
+        text: "I don't get how to do this with posts and tokens and whatnot. Also why would this site even need a user system, there would be no point! Well if you could download the games from this site then there would be, but it's easier to just publish stuff on steam. Anyway, this doesn't work so click OK already."
+    })
+}
